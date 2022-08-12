d3ae503119a5676bb1ec4b807ea06da00032f3a5
nuraleznur <nurderly@gmail.com>
2022-08-12 17:23:33 -0500

    segundo

--- ejercicio.py
+++ ejercicio.py
@@ -2,4 +2,5 @@ print(" 1 cambio Los mejores programadores")
 print(" 2 cambio Los mejores programadores"=
 
 
-print(" 3333 cambio Los mejores programadores"=
+print(" 3333 cambio Los mejores programadores")
+print("9999999")
